104e960e6a892e509edeb63f8bb1002ed8860ea2
Rafael Ventura <raf4ael@gmail.com>
2025-02-10 17:38:29 -0300

    feat(shared): implement error handling system

---
+++ backend/src/shared/errors/errorMessages.ts
@@ -0,0 +1,22 @@
+export const ERROR_MESSAGES = {
+    VALIDATION_ERROR: {
+        message: 'All fields are required',
+        internalCode: 'VALIDATION_ERROR',
+        statusCode: 400,
+    },
+    EMAIL_ALREADY_EXISTS: {
+        message: 'Email already exists',
+        internalCode: 'EMAIL_ALREADY_EXISTS',
+        statusCode: 400,
+    },
+    RA_ALREADY_EXISTS: {
+        message: 'RA already exists',
+        internalCode: 'RA_ALREADY_EXISTS',
+        statusCode: 400,
+    },
+    INTERNAL_SERVER_ERROR: {
+        message: 'Internal Server Error',
+        internalCode: 'INTERNAL_SERVER_ERROR',
+        statusCode: 500,
+    }
+};
