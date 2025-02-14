49d88e6d2aad30709d2d9b4cf3d89271e62a619c
Rafael Ventura <raf4ael@gmail.com>
2025-02-14 12:09:19 -0300

    feat: transforming the profile code into a single component

---
+++ frontend/components/Profile.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-sheet class="pa-4 sidebar-header">
+    <v-img src="https://maisaedu.com.br/hubfs/site-grupo-a/logo-mais-a-educacao.svg" class="logo" contain></v-img>
+
+    <v-avatar class="mb-2 avatar-overlay" size="64">
+      <v-img src="https://randomuser.me/api/portraits/lego/8.jpg"></v-img>
+    </v-avatar>
+
+    <div class="text-white user-email">john.doe@example.com</div>
+  </v-sheet>
+
+  <v-divider></v-divider>
+</template>
+
+<style scoped lang="scss">
+@use "@/styles/app.scss" as app;
+
+.sidebar-header {
+  text-align: center;
+  padding: 16px;
+  position: relative;
+  width: 100%;
+  background-color: app.$red-light;
+}
+
+.logo {
+  max-width: 30%;
+  margin: 0 auto 10%;
+}
+
+.avatar-overlay {
+  border: 3px solid white;
+  margin-top: 5%;
+}
+
+.user-email {
+  font-size: 1.2rem;
+  margin-top: 3%;
+}
+</style>
--- frontend/components/Sidebar.vue
+++ frontend/components/Sidebar.vue
@@ -7,38 +7,21 @@
       width="300"
       permanent
   >
-
-    <v-sheet class="pa-4 sidebar-header">
-      <v-img
-          src="https://maisaedu.com.br/hubfs/site-grupo-a/logo-mais-a-educacao.svg"
-          class="logo"
-          contain
-      ></v-img>
-
-      <v-avatar class="mb-2 avatar-overlay" size="64">
-        <v-img src="https://randomuser.me/api/portraits/lego/8.jpg"></v-img>
-      </v-avatar>
-
-      <div class="text-white user-email">john.doe@example.com</div>
-    </v-sheet>
-
-    <v-divider></v-divider>
+    <SidebarProfile/>
 
     <v-list-subheader class="subheader">Módulo Acadêmico</v-list-subheader>
 
     <v-list>
-      <v-list-item
-          prepend-icon="mdi-account-group"
-          title="Alunos"
-          to="/students"
-          class="nav-item"
-      ></v-list-item>
+      <v-list-item prepend-icon="mdi-account-group" title="Alunos" to="/students" class="nav-item"></v-list-item>
+
+      <v-list-item prepend-icon="mdi-plus" title="Cadastro" to="/students/register" class="nav-item"></v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
 
 <script setup>
 import {onMounted, ref} from "vue";
+import SidebarProfile from "@/components/Profile.vue";
 
 defineProps({
   drawer: Boolean,
@@ -52,48 +35,20 @@ onMounted(() => {
   isClient.value = true;
 });
 </script>
-<style scoped lang="scss">
-$red-primary: #c8102e;
-$red-dark: #a00d26;
-$red-light: #e63946;
-$grey-light: #f5f5f5;
 
-.v-sheet {
-  background-color: rgba($red-primary, 0.55);
-}
+<style scoped lang="scss">
+@use "@/styles/app.scss" as app;
 
 .v-navigation-drawer {
-  background-color: $grey-light;
+  background-color: app.$grey-light;
   width: 30%;
-}
-
-.sidebar-header {
-  text-align: center;
-  padding: 16px;
-  position: relative;
-  width: 100%;
-}
-
-.logo {
-  max-width: 30%;
-  margin: 0 auto 10%;
-}
-
-.avatar-overlay {
-  border: 3px solid white;
-  margin-top: 5%;
-}
-
-.user-email {
-  font-size: 1.2rem;
-  margin-top: 3%;
 }
 
 .nav-item {
   color: #333;
 
   &:hover {
-    background-color: rgba($red-light, 0.9);
+    background-color: rgba(app.$secondary-color, 0.9);
     color: white;
   }
 }
@@ -101,7 +56,7 @@ $grey-light: #f5f5f5;
 .subheader {
   padding-left: 10px;
   font-weight: bold;
-  color: $red-dark;
+  color: app.$red-dark;
   text-transform: uppercase;
 }
 </style>
